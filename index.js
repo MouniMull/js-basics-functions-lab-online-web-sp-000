a25eb24873a1ca3f0c473cbc88749817bc760e46
Mouni Mullapudi <mullapudi24@gmail.com>
2019-07-23 16:56:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Code your solution in this file!
+
+
+
+
+function distanceFromHqInBlocks(blocks){
+if (blocks > 42){
+  return blocks - 42;
+} else {
+  return 42 - blocks
+}
+}
+
+let feet = distanceFromHqInBlocks(blocks)*264;
+
+
+function distanceFromHqInFeet (blocks) {
+  let feet = distanceFromHqInBlocks(blocks)*264;
+  return feet;
+}
+
+function distanceTravelledInFeet(blocks, pickup) {
+  if( blocks > pickup){
+    return (blocks - pickup)*264;
+  } else {
+    return (pickup - blocks)*264;
+  }
+}
+
+
+function calculatesFarePrice (blocks, pickup) {
+  const distance = distanceTravelledInFeet(blocks, pickup);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return .02 * (distance - 400);
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
+
+
